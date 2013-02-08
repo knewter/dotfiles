752d49fb5aa6ba5690fc9f9a828036b34e3e83ea
Josh Adams <josh@isotope11.com>
2013-02-08 10:11:22 -0600

    Fix a thing I goofed up

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s `pwd`/vimrc ~/.vimrc
+ln -s `pwd`/vim/vimrc ~/.vimrc
 mkdir -p ~/vim/ftplugin
 ln -s `pwd`/vim/ftplugin/ruby.vim ~/vim/ftplugin/ruby.vim
 ln -s `pwd`/bash/aliases ~/.bash_files/aliases
