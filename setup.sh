0b11e6c6eaa83d0bc2615a4a639c1bf695e60e4f
Josh Adams <josh@isotope11.com>
2013-02-26 22:01:38 -0600

    Fix dumb ruby ftplugin bug

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ln -sf `pwd`/vim/vimrc ~/.vimrc
-mkdir -p ~/vim/ftplugin
-ln -sf `pwd`/vim/ftplugin/ruby.vim ~/vim/ftplugin/ruby.vim
+mkdir -p ~/.vim/ftplugin
+ln -sf `pwd`/vim/ftplugin/ruby.vim ~/.vim/ftplugin/ruby.vim
 ln -sf `pwd`/bash/aliases ~/.bash_files/aliases
 ln -sf `pwd`/ack/ackrc ~/.ackrc
 ln -sf `pwd`/xmodmap/xmodmap-esc ~/.xmodmap-esc
