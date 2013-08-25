607873f9e1ab5751f41b273c9108ef46acd8e0a3
Josh Adams <josh@isotope11.com>
2013-08-25 07:19:48 -0500

    Add some tmux keybindings to make it more vim

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 ln -sf `pwd`/vim/vimrc ~/.vimrc
 mkdir -p ~/.vim/ftplugin
 ln -sf `pwd`/vim/ftplugin/ruby.vim ~/.vim/ftplugin/ruby.vim
+ln -sf `pwd`/vim/ftplugin/erlang.vim ~/.vim/ftplugin/erlang.vim
 mkdir -p ~/.bash_files
 ln -sf `pwd`/bash/aliases ~/.bash_files/aliases
 ln -sf `pwd`/ack/ackrc ~/.ackrc
