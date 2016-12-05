9734fba545ffca8c47b4d3e6372564a5911b0042
Franzé Jr <franzejr@gmail.com>
2016-12-04 21:28:10 -0300

    Add Plug to be used with nvim

--- setup.sh
+++ setup.sh
@@ -43,6 +43,11 @@ ln -sf `pwd`/bin/subcount ~/bin/subcount
 # hyper
 ln -sf `pwd`/hyper/.hyper.js ~/.hyper.js
 
+# Plug (to be used with nvim)
+# https://github.com/junegunn/vim-plug
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # nvim
 mkdir -p ~/.config/nvim
 ln -sf `pwd`/nvim/init.vim ~/.config/nvim/
